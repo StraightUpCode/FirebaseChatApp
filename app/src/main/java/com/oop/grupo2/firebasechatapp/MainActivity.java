738e3e29c96a53c6a84cb2799e9a432ccc19fd91
Roberto <sanchez.roberto0810@gmail.com>
2018-11-06 20:53:41 -0600

    Login Agregado Menu "Opciones" para cerrar sesion

--- app/src/main/java/com/oop/grupo2/firebasechatapp/MainActivity.java
+++ app/src/main/java/com/oop/grupo2/firebasechatapp/MainActivity.java
@@ -1,27 +1,38 @@
 package com.oop.grupo2.firebasechatapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-    private String userId = "123456xD";
+    private String userId = "";
     Button submitButton;
     EditText messageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if( user == null){
+            startActivity(new Intent(this, SignInActivity.class));
 
+            finish();
         }
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+        userId = user.getUid();
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.submit);
         messageText = findViewById(R.id.message);
@@ -31,10 +42,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Message msg = new Message(userId, messageText.getText().toString());
                 db.collection("chat_publico").document("chat_general").collection("chat_msg").add(msg);
+                messageText.setText("");
 
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.sign_out){
+            signOut();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void signOut(){
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        userId = "";
+                        startActivity(new Intent(MainActivity.this, SignInActivity.class));
+                        finish();
+                    }
+                });
     }
 }
