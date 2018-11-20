74fe24e61b315a92e0d330bfa8134a3edcb04927
Roberto <sanchez.roberto0810@gmail.com>
2018-11-20 15:40:55 -0600

    Actividad que muestra los tabs Chat Privado y Chat Publico

---
+++ app/src/main/java/com/oop/grupo2/firebasechatapp/ChatRoomListActivity.java
@@ -0,0 +1,27 @@
+package com.oop.grupo2.firebasechatapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatRoomListActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room_list);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewPager=  (ViewPager) findViewById(R.id.viewPage_id);
+
+        TabManager tabManager = new TabManager(getSupportFragmentManager());
+        viewPager.setAdapter(tabManager);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+}
