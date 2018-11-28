1bf4f70621c2653b8cfe023669fbebc51d19b6fa
Roberto <sanchez.roberto0810@gmail.com>
2018-11-27 18:25:01 -0600

    Se creo el PrivateChatListFragment, que es un envoltorio para el ChatListFragment, pero contiene un buton que abre un pop up, para crear o unirse a un chat privado

---
+++ app/src/main/java/com/oop/grupo2/firebasechatapp/PrivateChatListFragment.java
@@ -0,0 +1,63 @@
+package com.oop.grupo2.firebasechatapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PrivateChatListFragment extends Fragment {
+    FragmentManager myFragmentManager;
+
+    public PrivateChatListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_private_chat_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        myFragmentManager = this.getChildFragmentManager();
+         ChatListFragment chatList = new ChatListFragment();
+        Bundle args = new Bundle();
+        args.putString(ChatListFragment.TIPO_DE_CHAT,"chat_publico");
+        chatList.setArguments(args);
+        myFragmentManager.beginTransaction()
+                .add(R.id.frame_holder,chatList,"Chat List Fragment")
+                .commit();
+
+
+        FloatingActionButton button = view.findViewById(R.id.floatingActionButton2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                myFragmentManager.beginTransaction()
+                        .add(R.id.frame_holder, new PopNewPrivateChat(),"popup")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myFragmentManager.popBackStack();
+            }
+        });
+    }
+}
