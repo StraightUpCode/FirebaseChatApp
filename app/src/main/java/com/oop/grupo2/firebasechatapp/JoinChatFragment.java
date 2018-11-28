e8619d0774cb617d679c30fe346bf8f181116f31
Roberto <sanchez.roberto0810@gmail.com>
2018-11-28 09:44:14 -0600

    Join Chat Fragment, todavia no hace nada pero es en el que se escribira el codigo para unirse a un chat

---
+++ app/src/main/java/com/oop/grupo2/firebasechatapp/JoinChatFragment.java
@@ -0,0 +1,29 @@
+package com.oop.grupo2.firebasechatapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JoinChatFragment extends Fragment {
+
+
+    public JoinChatFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_join_chat, container, false);
+    }
+
+}
